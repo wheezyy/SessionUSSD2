88abd32b99fd804c6018aec8e943b0f2bfcfc056
chogis <53309082+chogis@users.noreply.github.com>
2021-08-13 01:34:50 +0000

    Update USSDController.java

--- src/android/USSDController.java
+++ src/android/USSDController.java
@@ -4,7 +4,7 @@
  * porfile.romellfudi.com
  */
 
-package com.romellfudi.ussdlibrary;
+package com.ramymokako.plugin.ussd.android;
 
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.annotation.SuppressLint;
