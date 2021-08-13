acd7d0d36d5cb60eb6f41c26fa272ddc4926f3eb
chogis <53309082+chogis@users.noreply.github.com>
2021-08-13 01:37:09 +0000

    Update GifImageView.java

--- src/android/GifImageView.java
+++ src/android/GifImageView.java
@@ -4,7 +4,7 @@
  * porfile.romellfudi.com
  */
 
-package com.romellfudi.ussdlibrary;
+package com.ramymokako.plugin.ussd.android;
 
 import android.content.Context;
 import android.graphics.Canvas;
