80be998aa580e22568a207a716e8f376e4cca8a8
chogis <53309082+chogis@users.noreply.github.com>
2021-08-13 01:08:41 +0000

    Update USSDInterface.java

--- src/android/USSDInterface.java
+++ src/android/USSDInterface.java
@@ -1,21 +1,19 @@
-package com.ramymokako.plugin.ussd.android;
+/*
+ * Copyright (c) 2020. BoostTag E.I.R.L. Romell D.Z.
+ * All rights reserved
+ * porfile.romellfudi.com
+ */
+
+package com.romellfudi.ussdlibrary;
+
+/**
+ * Interface ussd handler
+ *
+ * @version 1.1.c
+ * @author Romell Dominguez
+ * @version 1.1.c 0/4/18
+ */
 
 public interface USSDInterface {
-    void sendData(String str);
+    void sendData(String text);
 }
-
-// //package com.romellfudi.ussdlibrary;
-
-// package com.ramymokako.plugin.ussd.android;
-
-// /**
-//  * Interface ussd handler
-//  *
-//  * @version 1.1.c
-//  * @author Romell Dominguez
-//  * @version 1.1.c 0/4/18
-//  */
-
-// public interface USSDInterface {
-//     void sendData(String text);
-// }
