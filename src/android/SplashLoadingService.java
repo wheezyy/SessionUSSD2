a6d0e078ad9f0c56282a108abc15f1c2985fe52e
chogis <53309082+chogis@users.noreply.github.com>
2021-08-13 00:59:32 +0000

    Update SplashLoadingService.java

--- src/android/SplashLoadingService.java
+++ src/android/SplashLoadingService.java
@@ -1,123 +1,195 @@
-//package com.romellfudi.ussdlibrary;
-
 package com.ramymokako.plugin.ussd.android;
 
-import android.annotation.SuppressLint;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
-import android.view.Gravity;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-import android.app.Application;
-import android.content.res.Resources;
-
-/**
- * SplashLoadingService for Android splashing dialog
- *
- * @author Romell Dominguez
- * @version 1.1.d 23/02/2017
- * @since 1.1.d
- */
 public class SplashLoadingService extends Service {
+    /* access modifiers changed from: private */
+    public LinearLayout layout;
+    /* access modifiers changed from: private */
 
-    private LinearLayout layout;
-    private WindowManager wm;
+    /* renamed from: wm */
+    public WindowManager f1wm;
 
-    @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
-    @SuppressLint("ResourceAsColor")
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        Point size = new Point();
-        wm.getDefaultDisplay().getSize(size);
-        int LAYOUT_FLAG;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-        } else {
-            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
-        }
-
-        int padding_in_dp = 100;
-        final float scale = getResources().getDisplayMetrics().density;
-        int padding_in_px = (int) (padding_in_dp * scale + 0.5f);
-
-        layout = new LinearLayout(this);
-        //layout.setBackgroundColor(R.color.blue_background);
-        layout.setOrientation(LinearLayout.VERTICAL);
-
-        WindowManager.LayoutParams params =
-                new WindowManager.LayoutParams
-                        (WindowManager.LayoutParams.MATCH_PARENT,
-                                WindowManager.LayoutParams.MATCH_PARENT,
-                                LAYOUT_FLAG
-                                , WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-                                PixelFormat.RGB_565);
-
+    public int onStartCommand(Intent intent, int i, int i2) {
+        this.f1wm = (WindowManager) getSystemService("window");
+        this.f1wm.getDefaultDisplay().getSize(new Point());
+        int i3 = Build.VERSION.SDK_INT >= 26 ? 2038 : 2002;
+        int i4 = (int) ((((float) 100) * getResources().getDisplayMetrics().density) + 0.5f);
+        LinearLayout linearLayout = new LinearLayout(this);
+        this.layout = linearLayout;
+        linearLayout.setOrientation(1);
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(-1, -1, i3, 40, 4);
         ImageView imageView = new ImageView(this);
-        //imageView.setImageResource(R.drawable.favico);
-        imageView.setPaddingRelative(0,padding_in_px,0,padding_in_px);
-        LinearLayout.LayoutParams params_ll = new LinearLayout
-                .LayoutParams(LinearLayout.MarginLayoutParams.MATCH_PARENT, 0);
-        params_ll.gravity = Gravity.CENTER;
-        params_ll.weight = 1;
-
+        imageView.setPaddingRelative(0, i4, 0, i4);
+        LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(-1, 0);
+        layoutParams2.gravity = 17;
+        layoutParams2.weight = 1.0f;
         RelativeLayout relativeLayout = new RelativeLayout(this);
-        RelativeLayout.LayoutParams rp = new RelativeLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT);
-        rp.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-        relativeLayout.addView(imageView,rp);
-        layout.addView(relativeLayout, params_ll);
-
+        RelativeLayout.LayoutParams layoutParams3 = new RelativeLayout.LayoutParams(-1, -1);
+        layoutParams3.addRule(13, -1);
+        relativeLayout.addView(imageView, layoutParams3);
+        this.layout.addView(relativeLayout, layoutParams2);
         GifImageView gifImageView = new GifImageView(this);
-        //gifImageView.setGifImageResource(R.drawable.loading);
-        gifImageView.setPaddingRelative(0,padding_in_px,0,padding_in_px);
-
-        relativeLayout = new RelativeLayout(this);
-        rp = new RelativeLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT);
-        rp.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-        relativeLayout.addView(gifImageView,rp);
-
-        layout.addView(relativeLayout,params_ll);
-
-        wm.addView(layout,params);
-        return START_STICKY;
+        gifImageView.setPaddingRelative(0, i4, 0, i4);
+        RelativeLayout relativeLayout2 = new RelativeLayout(this);
+        RelativeLayout.LayoutParams layoutParams4 = new RelativeLayout.LayoutParams(-1, -1);
+        layoutParams4.addRule(13, -1);
+        relativeLayout2.addView(gifImageView, layoutParams4);
+        this.layout.addView(relativeLayout2, layoutParams2);
+        this.f1wm.addView(this.layout, layoutParams);
+        return 1;
     }
 
-    @Override
     public void onCreate() {
         super.onCreate();
-
     }
 
-    @Override
     public void onDestroy() {
         super.onDestroy();
         new Handler().postDelayed(new Runnable() {
-            @Override
             public void run() {
-                if (layout != null) {
-                    wm.removeView(layout);
-                    layout = null;
+                if (SplashLoadingService.this.layout != null) {
+                    SplashLoadingService.this.f1wm.removeView(SplashLoadingService.this.layout);
+                    LinearLayout unused = SplashLoadingService.this.layout = null;
                 }
             }
-        },500);
+        }, 500);
     }
-
 }
+
+// //package com.romellfudi.ussdlibrary;
+
+// package com.ramymokako.plugin.ussd.android;
+
+// import android.annotation.SuppressLint;
+// import android.app.Service;
+// import android.content.Context;
+// import android.content.Intent;
+// import android.graphics.PixelFormat;
+// import android.graphics.Point;
+// import android.os.Build;
+// import android.os.Handler;
+// import android.os.IBinder;
+// import android.view.Gravity;
+// import android.view.ViewGroup;
+// import android.view.WindowManager;
+// import android.widget.ImageView;
+// import android.widget.LinearLayout;
+// import android.widget.RelativeLayout;
+
+// import android.app.Application;
+// import android.content.res.Resources;
+
+// /**
+//  * SplashLoadingService for Android splashing dialog
+//  *
+//  * @author Romell Dominguez
+//  * @version 1.1.d 23/02/2017
+//  * @since 1.1.d
+//  */
+// public class SplashLoadingService extends Service {
+
+//     private LinearLayout layout;
+//     private WindowManager wm;
+
+//     @Override
+//     public IBinder onBind(Intent intent) {
+//         return null;
+//     }
+
+//     @SuppressLint("ResourceAsColor")
+//     public int onStartCommand(Intent intent, int flags, int startId) {
+//         wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+//         Point size = new Point();
+//         wm.getDefaultDisplay().getSize(size);
+//         int LAYOUT_FLAG;
+//         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//             LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+//         } else {
+//             LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
+//         }
+
+//         int padding_in_dp = 100;
+//         final float scale = getResources().getDisplayMetrics().density;
+//         int padding_in_px = (int) (padding_in_dp * scale + 0.5f);
+
+//         layout = new LinearLayout(this);
+//         //layout.setBackgroundColor(R.color.blue_background);
+//         layout.setOrientation(LinearLayout.VERTICAL);
+
+//         WindowManager.LayoutParams params =
+//                 new WindowManager.LayoutParams
+//                         (WindowManager.LayoutParams.MATCH_PARENT,
+//                                 WindowManager.LayoutParams.MATCH_PARENT,
+//                                 LAYOUT_FLAG
+//                                 , WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+//                                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+//                                 PixelFormat.RGB_565);
+
+//         ImageView imageView = new ImageView(this);
+//         //imageView.setImageResource(R.drawable.favico);
+//         imageView.setPaddingRelative(0,padding_in_px,0,padding_in_px);
+//         LinearLayout.LayoutParams params_ll = new LinearLayout
+//                 .LayoutParams(LinearLayout.MarginLayoutParams.MATCH_PARENT, 0);
+//         params_ll.gravity = Gravity.CENTER;
+//         params_ll.weight = 1;
+
+//         RelativeLayout relativeLayout = new RelativeLayout(this);
+//         RelativeLayout.LayoutParams rp = new RelativeLayout.LayoutParams(
+//                 ViewGroup.LayoutParams.MATCH_PARENT,
+//                 ViewGroup.LayoutParams.MATCH_PARENT);
+//         rp.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+//         relativeLayout.addView(imageView,rp);
+//         layout.addView(relativeLayout, params_ll);
+
+//         GifImageView gifImageView = new GifImageView(this);
+//         //gifImageView.setGifImageResource(R.drawable.loading);
+//         gifImageView.setPaddingRelative(0,padding_in_px,0,padding_in_px);
+
+//         relativeLayout = new RelativeLayout(this);
+//         rp = new RelativeLayout.LayoutParams(
+//                 ViewGroup.LayoutParams.MATCH_PARENT,
+//                 ViewGroup.LayoutParams.MATCH_PARENT);
+//         rp.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+//         relativeLayout.addView(gifImageView,rp);
+
+//         layout.addView(relativeLayout,params_ll);
+
+//         wm.addView(layout,params);
+//         return START_STICKY;
+//     }
+
+//     @Override
+//     public void onCreate() {
+//         super.onCreate();
+
+//     }
+
+//     @Override
+//     public void onDestroy() {
+//         super.onDestroy();
+//         new Handler().postDelayed(new Runnable() {
+//             @Override
+//             public void run() {
+//                 if (layout != null) {
+//                     wm.removeView(layout);
+//                     layout = null;
+//                 }
+//             }
+//         },500);
+//     }
+
+// }
