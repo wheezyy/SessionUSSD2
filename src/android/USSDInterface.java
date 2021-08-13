af6558f3de8fc9aa11d292ba4071d65a4a1b84f5
chogis <53309082+chogis@users.noreply.github.com>
2021-08-13 00:57:39 +0000

    Update USSDInterface.java

--- src/android/USSDInterface.java
+++ src/android/USSDInterface.java
@@ -1,15 +1,21 @@
-//package com.romellfudi.ussdlibrary;
-
 package com.ramymokako.plugin.ussd.android;
 
-/**
- * Interface ussd handler
- *
- * @version 1.1.c
- * @author Romell Dominguez
- * @version 1.1.c 0/4/18
- */
-
 public interface USSDInterface {
-    void sendData(String text);
+    void sendData(String str);
 }
+
+// //package com.romellfudi.ussdlibrary;
+
+// package com.ramymokako.plugin.ussd.android;
+
+// /**
+//  * Interface ussd handler
+//  *
+//  * @version 1.1.c
+//  * @author Romell Dominguez
+//  * @version 1.1.c 0/4/18
+//  */
+
+// public interface USSDInterface {
+//     void sendData(String text);
+// }
