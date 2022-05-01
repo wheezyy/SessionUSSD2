b17b7a17567fbc5e588d9926fead762dd764b512
Willy Rotich <k.rotich15@students.dkut.ac.ke>
2022-05-01 04:21:17 +0300

    Update VoIpUSSD.java

--- src/android/VoIpUSSD.java
+++ src/android/VoIpUSSD.java
@@ -81,10 +81,12 @@ public class VoIpUSSD extends CordovaPlugin {
             ussdApi.send(ussdCommands[index], new USSDController.CallbackMessage() {
                 @Override
                 public void responseMessage(String message) {
-                    if(index == ussdCommands.length - 2){
-                        message = message + "DONEENOD";
-			    ussdApi.cancel();
-                    }
+//                     if(index == ussdCommands.length - 2){
+//                         message = message + "DONEENOD";
+// 			    ussdApi.cancel();
+//                     }
+			
+			ussdApi.cancel();
                     Log.d(TAG, "###doUSSD### Received ### " + index + ": " + message);
                     pluginResult[index]  = new PluginResult(PluginResult.Status.OK, ussdCommands[index] + ": " + message);
                     pluginResult[index].setKeepCallback(true);
